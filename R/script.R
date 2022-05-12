a19ab5c97d07e307d7a706fc85317ea9b155f1a0
Arni Magnusson <thisisarni@gmail.com>
2022-05-13 08:59:37 +1100

    Plot last years of SB

--- R/script.R
+++ R/script.R
@@ -12,9 +12,10 @@ penguin.diagnostic.12 <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnost
 penguin.diagnostic <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnostic/plot-14.par.rep")
 penguin.grid <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Grid/CondLen_M0.2_Size60_H0.8_Mix2/plot-out.par.rep")
 web.zip.file <- read.MFCLRep("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-yft-2020-grid/main/Grid_Models/CondAgeGrowth_Size60_H0.8_Mix2/plot-out.par.rep")
-download("https://github.com/PacificCommunity/ofp-sam-yft-2020-runs/releases/download/runs/diagnostic_matt.zip", "run")
+if(!file.exists("run/diagnostic_matt.zip"))
+  download("https://github.com/PacificCommunity/ofp-sam-yft-2020-runs/releases/download/runs/diagnostic_matt.zip", "run")
 our.condor.run <- read.MFCLRep(unz("run/diagnostic_matt.zip", "plot-10.par.rep"))
-john.yft.diag <- read.MFCLRep("c:/x/yft/john_yft_diag/plot-14.par.rep")
+john.yft.diag <- read.MFCLRep("c:/x/yft/john_yft_diag/plot-15.par.rep")
 
 ## 2  Plot SB and SBF0
 
@@ -22,45 +23,50 @@ xyplot(data~year|area, group=qname,
        data=FLQuants(qts(adultBiomass_nofish(our.condor.run)), qts(adultBiomass(our.condor.run))),
        type="l", as.table=TRUE, main="Our Condor run")
 
-## xyplot(data~year, data=FLQuants(SB(our.condor.run), SBF0(our.condor.run)))
+xyplot(data~year, data=FLQuants(SB(our.condor.run), SBF0(our.condor.run)))
 
-b <- flr2taf(SB(penguin.diagnostic.10)/1e6, colname="SB")
-b0 <- flr2taf(SBF0(penguin.diagnostic.10)/1e6, colname="SBF0")
+b <- flr2taf(SB(penguin.diagnostic.10)/1e3, colname="SB")
+b0 <- flr2taf(SBF0(penguin.diagnostic.10)/1e3, colname="SBF0")
 pdiag10 <- merge(b, b0)
 pdiag10$Depletion <- pdiag10$SB / pdiag10$SBF0
 
-b <- flr2taf(SB(penguin.diagnostic.12)/1e6, colname="SB")
-b0 <- flr2taf(SBF0(penguin.diagnostic.12)/1e6, colname="SBF0")
+b <- flr2taf(SB(penguin.diagnostic.12)/1e3, colname="SB")
+b0 <- flr2taf(SBF0(penguin.diagnostic.12)/1e3, colname="SBF0")
 pdiag12 <- merge(b, b0)
 pdiag12$Depletion <- pdiag12$SB / pdiag12$SBF0
 
-b <- flr2taf(SB(penguin.diagnostic)/1e6, colname="SB")
-b0 <- flr2taf(SBF0(penguin.diagnostic)/1e6, colname="SBF0")
+b <- flr2taf(SB(penguin.diagnostic)/1e3, colname="SB")
+b0 <- flr2taf(SBF0(penguin.diagnostic)/1e3, colname="SBF0")
 pdiag <- merge(b, b0)
 pdiag$Depletion <- pdiag$SB / pdiag$SBF0
 
-b <- flr2taf(SB(penguin.grid)/1e6, colname="SB")
-b0 <- flr2taf(SBF0(penguin.grid)/1e6, colname="SBF0")
+b <- flr2taf(SB(penguin.grid)/1e3, colname="SB")
+b0 <- flr2taf(SBF0(penguin.grid)/1e3, colname="SBF0")
 pgrid <- merge(b, b0)
 pgrid$Depletion <- pgrid$SB / pgrid$SBF0
 
-b <- flr2taf(SB(web.zip.file)/1e6, colname="SB")
-b0 <- flr2taf(SBF0(web.zip.file)/1e6, colname="SBF0")
+b <- flr2taf(SB(web.zip.file)/1e3, colname="SB")
+b0 <- flr2taf(SBF0(web.zip.file)/1e3, colname="SBF0")
 web <- merge(b, b0)
 web$Depletion <- web$SB / web$SBF0
 
-b <- flr2taf(SB(our.condor.run)/1e6, colname="SB")
-b0 <- flr2taf(SBF0(our.condor.run)/1e6, colname="SBF0")
+b <- flr2taf(SB(our.condor.run)/1e3, colname="SB")
+b0 <- flr2taf(SBF0(our.condor.run)/1e3, colname="SBF0")
 our <- merge(b, b0)
 our$Depletion <- our$SB / our$SBF0
 
 sb <- data.frame(Year=our$Year, pdiag10=pdiag10$SB, pdiag12=pdiag12$SB, pdiag=pdiag$SB, pgrid=pgrid$SB, web=web$SB, our=our$SB)
 depletion <- data.frame(Year=our$Year, pdiag10=pdiag10$Depletion, pdiag12=pdiag12$Depletion, pdiag=pdiag$Depletion, pgrid=pgrid$Depletion, web=web$Depletion, our=our$Depletion)
-round(sb, 3)
-round(depletion, 3)
-## round(sb$web / sb$our, 2)
+round(sb)
+round(depletion, 4)
 
 matplot(sb[1], sb[-1], type="l", lty=1, lwd=3, ylim=lim(sb[-1]), xlab="Year", ylab="SB")
+matplot(sb[1], sb[-1], type="l", lty=1, lwd=3, xlab="Year", ylab="SB (kt)", xlim=c(2010,2018), ylim=c(1700,2300))
+abline(h=seq(1700,2300,by=100), lty=3, col="gray")
+round(tail(sb,1))
+xyplot(SB~Year, groups=Model, data=taf2long(sb, c("Year","Model","SB")), type="l", ylim=lim(sb[-1]), lwd=3, col=1:6)
+xyplot(SB~Year, groups=Model, data=taf2long(sb, c("Year","Model","SB")), subset=Year>=2010, type="l", lwd=3, col=1:6)
+
 matplot(depletion[1], depletion[-1], type="l", lty=1, lwd=3, ylim=lim(depletion[-1]), xlab="Year", ylab="SB/SBF0")
 
 plot(SBF0~Year, data=our, type="l", lty=3, ylim=lim(our$SBF0), ylab="Spawning biomass (Mt)")
