0479154e3b8e5799f0206ea27bd7e6d94900b8bb
Arni Magnusson <thisisarni@gmail.com>
2022-05-12 17:41:05 +1100

    Compare diagnostic 10.par, 12.par, 14.par

--- R/script.R
+++ R/script.R
@@ -7,11 +7,14 @@ library(FLR4MFCL)
 
 mkdir("run")
 
+penguin.diagnostic.10 <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnostic/plot-10.par.rep")
+penguin.diagnostic.12 <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnostic/plot-12.par.rep")
 penguin.diagnostic <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnostic/plot-14.par.rep")
 penguin.grid <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Grid/CondLen_M0.2_Size60_H0.8_Mix2/plot-out.par.rep")
-web.zip.file <- read.MFCLRep("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-yft-2020-grid/main/Grid_Models/CondAgeGrowth_Size20_H0.8_Mix2/plot-out.par.rep")
+web.zip.file <- read.MFCLRep("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-yft-2020-grid/main/Grid_Models/CondAgeGrowth_Size60_H0.8_Mix2/plot-out.par.rep")
 download("https://github.com/PacificCommunity/ofp-sam-yft-2020-runs/releases/download/runs/diagnostic_matt.zip", "run")
 our.condor.run <- read.MFCLRep(unz("run/diagnostic_matt.zip", "plot-10.par.rep"))
+john.yft.diag <- read.MFCLRep("c:/x/yft/john_yft_diag/plot-14.par.rep")
 
 ## 2  Plot SB and SBF0
 
@@ -20,6 +23,16 @@ xyplot(data~year|area, group=qname,
        type="l", as.table=TRUE, main="Our Condor run")
 
 ## xyplot(data~year, data=FLQuants(SB(our.condor.run), SBF0(our.condor.run)))
+
+b <- flr2taf(SB(penguin.diagnostic.10)/1e6, colname="SB")
+b0 <- flr2taf(SBF0(penguin.diagnostic.10)/1e6, colname="SBF0")
+pdiag10 <- merge(b, b0)
+pdiag10$Depletion <- pdiag10$SB / pdiag10$SBF0
+
+b <- flr2taf(SB(penguin.diagnostic.12)/1e6, colname="SB")
+b0 <- flr2taf(SBF0(penguin.diagnostic.12)/1e6, colname="SBF0")
+pdiag12 <- merge(b, b0)
+pdiag12$Depletion <- pdiag12$SB / pdiag12$SBF0
 
 b <- flr2taf(SB(penguin.diagnostic)/1e6, colname="SB")
 b0 <- flr2taf(SBF0(penguin.diagnostic)/1e6, colname="SBF0")
@@ -41,11 +54,14 @@ b0 <- flr2taf(SBF0(our.condor.run)/1e6, colname="SBF0")
 our <- merge(b, b0)
 our$Depletion <- our$SB / our$SBF0
 
-sb <- data.frame(Year=our$Year, pdiag=pdiag$SB, pgrid=pgrid$SB, web=web$SB, our=our$SB)
-depletion <- data.frame(Year=our$Year, pdiag=pdiag$Depletion, pgrid=pgrid$Depletion, web=web$Depletion, our=our$Depletion)
+sb <- data.frame(Year=our$Year, pdiag10=pdiag10$SB, pdiag12=pdiag12$SB, pdiag=pdiag$SB, pgrid=pgrid$SB, web=web$SB, our=our$SB)
+depletion <- data.frame(Year=our$Year, pdiag10=pdiag10$Depletion, pdiag12=pdiag12$Depletion, pdiag=pdiag$Depletion, pgrid=pgrid$Depletion, web=web$Depletion, our=our$Depletion)
 round(sb, 3)
 round(depletion, 3)
-round(sb$web / sb$our, 2)
+## round(sb$web / sb$our, 2)
+
+matplot(sb[1], sb[-1], type="l", lty=1, lwd=3, ylim=lim(sb[-1]), xlab="Year", ylab="SB")
+matplot(depletion[1], depletion[-1], type="l", lty=1, lwd=3, ylim=lim(depletion[-1]), xlab="Year", ylab="SB/SBF0")
 
 plot(SBF0~Year, data=our, type="l", lty=3, ylim=lim(our$SBF0), ylab="Spawning biomass (Mt)")
 lines(SB~Year, data=our)
